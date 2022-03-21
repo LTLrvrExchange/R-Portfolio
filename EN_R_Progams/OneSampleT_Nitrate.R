f7b75e8822a3fdfdb89e5a94f18f335d23c9a5fc
Redwood Valley Rancheria <96074474+LTLrvrExchange@users.noreply.github.com>
2022-03-21 14:06:57 -0700

    Update OneSampleT_Nitrate.R

--- EN_R_Progams/OneSampleT_Nitrate.R
+++ EN_R_Progams/OneSampleT_Nitrate.R
@@ -1,4 +1,4 @@
-# One-sample T-test for Nitrate data
+# One-sample T-test for Nitrate data - this program will be updated to tidyverse
 
 
 # Load necessary libraries
@@ -7,22 +7,22 @@ library(psych)
 
 # Confidence intervals for sigma^2
 cisig2 <- function(y,alpha=0.05){
-s2 <- var(y)
-n <- length(y)
-lower <- (n-1)*s2/qchisq(1-alpha/2,n-1)
-upper <- (n-1)*s2/qchisq(alpha/2,n-1)
-return(c(lower,upper))
+  s2 <- var(y)
+  n <- length(y)
+  lower <- (n-1)*s2/qchisq(1-alpha/2,n-1)
+  upper <- (n-1)*s2/qchisq(alpha/2,n-1)
+  return(c(lower,upper))
 }
 
 getwd()
-setwd("H:/My Drive/02EnvirExchNetwkGrant/ENGrantDueProducts/EN_R_DataSources")
+setwd("C:/put/pathto/data/here")
 
 # Read in data set
 Nitrate<- read_csv("OneSampleT_Nitrates.csv",
-                      col_names = TRUE, 
-                      cols(
-                      mgl = col_integer()
-                      ))
+                   col_names = TRUE, 
+                   cols(
+                     mgl = col_integer()
+                   ))
 
 
 # Print the data
@@ -32,7 +32,7 @@ print(Nitrate)
 
 # Graphics using ggplot2
 G1 <- ggplot(Nitrate,aes(x=mgl))+
-      geom_histogram(binwidth=1)
+  geom_histogram(binwidth=1)
 
 G1
 
@@ -61,5 +61,3 @@ print(cisig2out)
 cat("95 percent confidence interval for sigma:\n")
 print(sqrt(cisig2out))
 detach(Nitrate)
-
-
